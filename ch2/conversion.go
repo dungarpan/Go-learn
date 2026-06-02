2c948fbec8fe752861c1105ca775cedcde531f70
Arpan Chatterjee <arpanchatterjeeg@gmail.com>
2026-06-02 14:17:10 -0400

    Add explicit type conversion for int and byte

--- ch2/conversion.go
+++ ch2/conversion.go
@@ -1,13 +1,7 @@
 package main
 
-import "fmt"
-
 var x int = 42
 var y float64 = 30.2
 var sum1 float64 = float64(x) + y
 var sum2 int = x + int(y)
 
-func main() {
-	fmt.Println(sum1, sum2)
-	fmt.Println(sum3, sum4)
-}
---
+++ ch2/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum1, sum2)
+	fmt.Println(sum3, sum4)
+}
